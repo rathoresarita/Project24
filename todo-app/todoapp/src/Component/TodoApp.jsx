8d29b51e478d6b0ffafb6376284193fdba7202c2
rathoresarita <your.saritasinghrathore140@gmail.com>
2024-05-23 12:24:43 +0530

    added

--- todo-app/todoapp/src/Component/TodoApp.jsx
+++ todo-app/todoapp/src/Component/TodoApp.jsx
@@ -1,14 +1,20 @@
 import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { addtoDo, completeToDo, deleteToDo } from '../Reducer/action'
 
 import '../css/app.css'
 import TodoList from './TodoList'
 const TodoApp = () => {
-    const [name, setName] = useState(null)
-    const [des, setDes] = useState(null)
-    const [tasks, setTasks] = useState('')
+    const [name, setName] = useState('')
+    const [des, setDes] = useState('')
+    // const [tasks, setTasks] = useState('')
 
-    const [counter, setCounter] = useState(0);
+    // const [counter, setCounter] = useState(0);
     const [showLists, setShowLists] = useState(false);
+
+
+    const tasks = useSelector(state => state.tasks);
+    const dispatch = useDispatch();
 
     const handleInputName = (e) => {
         setName(e.target.value)
@@ -24,14 +30,18 @@ const TodoApp = () => {
     }
     const handleDelete = (index) => {
 
-        const newTask = [...tasks]
-        newTask.splice(index, 1)
-        setTasks(newTask)
+        // const newTask = [...tasks]
+        // newTask.splice(index, 1)
+        // setTasks(newTask)
+        dispatch(deleteToDo(index))
+
     }
     const handleAdd = () => {
         if (name.trim() !== '' && des.trim() !== '') {
-            const newTask = { name, des };
-            setTasks(prevTasks => [...prevTasks, newTask]); // Updating tasks using previous state
+            // const newTask = { name, des };
+            // setTasks(prevTasks => [...prevTasks, newTask]); // Updating tasks using previous state
+
+            dispatch(addtoDo({ name, des }));
             setName('');
             setDes('');
             setShowLists(true); // Update showList here to true after adding a task
@@ -39,11 +49,14 @@ const TodoApp = () => {
     };
     console.log('tssks', tasks)
     const handleComplete = (index) => {
-       const newTask=[...tasks]
-       newTask[index].completed=true
-       setTasks(newTask)
+        //    const newTask=[...tasks]
+        //    newTask[index].completed=true
+        //    setTasks(newTask)
+
+
+        dispatch(completeToDo(index))
     };
-    
+
 
 
 
@@ -72,7 +85,7 @@ const TodoApp = () => {
 
                 </div>
             </div>
-            {showLists && <TodoList tasks={tasks} onDelete={handleDelete} onComplete={handleComplete} ></TodoList>
+            {showLists && <TodoList tasks={tasks} onDelete={handleDelete} onComplete={handleComplete}></TodoList>
 
 
 
--- todo-app/todoapp/src/Component/TodoList.jsx
+++ todo-app/todoapp/src/Component/TodoList.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import '../css/list.css';
 
-const TodoList = ({ tasks, onDelete ,onComplete}) => {
+const TodoList = ({ tasks, onDelete, onComplete }) => {
   console.log('taskkkksss', tasks);
 
   return (
@@ -11,15 +11,15 @@ const TodoList = ({ tasks, onDelete ,onComplete}) => {
 
         <div className='todoList' key={index}>
           <div className='listName'>
-          {/* Conditionally apply strike-through style if task is completed */}
-          <p style={{ textDecoration: task.completed ? 'line-through' : 'none' }}>{task.name}</p>
- 
+            {/* Conditionally apply strike-through style if task is completed */}
+            <p style={{ textDecoration: task.completed ? 'line-through' : 'none' }}>{task.name}</p>
+
           </div>
           <div className='listName'>
-          <p style={{ textDecoration: task.completed ? 'line-through' : 'none' }}>{task.des}</p>
+            <p style={{ textDecoration: task.completed ? 'line-through' : 'none' }}>{task.des}</p>
           </div>
           <div className='listButtons'>
-            <button className='button1 ' onClick={()=>onComplete(index)} >Complete</button>
+            <button className='button1 ' onClick={() => onComplete(index)} >Complete</button>
             <button className='button2' onClick={() => onDelete(index)}>Delete</button>
           </div>
         </div>
